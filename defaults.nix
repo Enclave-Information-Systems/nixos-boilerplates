1ef321f2cf5f41d145ca454dc747648fba40ff0f
ryan.mcgee <ryan@enclaveis.com>
2023-09-06 16:03:46 -0500

    updated defaults to read from variables file

--- defaults.nix
+++ defaults.nix
@@ -1,5 +1,8 @@
 { inputs, outputs, config, lib, ... }:
-
+let
+  vars = import ./vars.nix;
+  user = vars.user;
+in
 {
   nix = {
     settings.experimental-features = "nix-command flakes";
@@ -18,6 +21,29 @@
     # This will additionally add your inputs to the system's legacy channels
     # Making legacy nix commands consistent as well, awesome!
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+  };
+
+  # Define a user account. Don't forget to set a password with 'passwd'.
+  users.users.${user} = lib.mkForce {
+    isNormalUser = true;
+    description = "${user}";
+    extraGroups = [ "networkmanager" "wheel" "${user}"];
+  };
+
+
+  networking.hostName = lib.mkForce vars.hostname;
+  time.timeZone = lib.mkForce vars.tz;
+  i18n.defaultLocale = lib.mkForce vars.locale;
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = lib.mkForce vars.locale;
+    LC_IDENTIFICATION = lib.mkForce vars.locale;
+    LC_MEASUREMENT = lib.mkForce vars.locale;
+    LC_MONETARY = lib.mkForce vars.locale;
+    LC_NAME = lib.mkForce vars.locale;
+    LC_NUMERIC = lib.mkForce vars.locale;
+    LC_PAPER = lib.mkForce vars.locale;
+    LC_TELEPHONE = lib.mkForce vars.locale;
+    LC_TIME = lib.mkForce vars.locale;
   };
 
   #Enable Automatic Updating
