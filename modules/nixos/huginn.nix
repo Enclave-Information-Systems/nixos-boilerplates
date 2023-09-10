cfa2b18ff743429d233113eadf6e1de8a777292e
eli <elicastillo203@gmail.com>
2023-09-10 19:47:37 +0300

    Added the correct volume for Huginn SQL database

--- modules/nixos/huginn.nix
+++ modules/nixos/huginn.nix
@@ -4,7 +4,7 @@ let vars = import ../../vars.nix; in
   # Home Assistant
   virtualisation.oci-containers.containers."huginn" = {
       autoStart = true;  
-      volumes = [ "huginn:/config" ];
+      volumes = [ "/home/huginn/mysql-data:/var/lib/mysql" ];
       environment.TZ = vars.tz;
       image = "ghcr.io/huginn/huginn:latest"; # Warning: if the tag does not change, the image will not be updated
       extraOptions = [ 
