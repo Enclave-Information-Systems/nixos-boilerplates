6d3712cd5cd2dbbb598055772e212b9a27053927
eli <elicastillo203@gmail.com>
2023-09-10 18:23:52 +0300

    Adding recommended changes to hass.nix for pull request

--- modules/nixos/hass.nix
+++ modules/nixos/hass.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
-
+let vars = import ./vars.nix;
 {
   # Home Assistant
   virtualisation.oci-containers.containers."homeassistant" = {
       autoStart = true;  
       volumes = [ "home-assistant:/config" ];
-      environment.TZ = "Europe/Athens";
+      environment.TZ = vars.tz;
       image = "ghcr.io/home-assistant/home-assistant:stable"; # Warning: if the tag does not change, the image will not be updated
       extraOptions = [ 
         "--network=host" 
