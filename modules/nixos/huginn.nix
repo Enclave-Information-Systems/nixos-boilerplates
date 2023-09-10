56ae1b085c055bbad410a34f41b55c0ef3e3b6ef
eli <elicastillo203@gmail.com>
2023-09-10 18:23:30 +0300

    Adding recommended changes to huginn.nix for pull request

--- modules/nixos/huginn.nix
+++ modules/nixos/huginn.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
-
+let vars = import ./vars.nix;
 {
   # Home Assistant
   virtualisation.oci-containers.containers."huginn" = {
       autoStart = true;  
       volumes = [ "huginn:/config" ];
-      environment.TZ = "Europe/Athens";
+      environment.TZ = vars.tz;
       image = "ghcr.io/huginn/huginn:latest"; # Warning: if the tag does not change, the image will not be updated
       extraOptions = [ 
         "--network=host" 
